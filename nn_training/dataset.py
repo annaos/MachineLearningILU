18c74fd76d77bc2350365359c61e6b213965593c
Anna Ostrovskaya <admin@annaos.dev>
2022-04-30 18:17:20 +0200

    add reduced features for dataset

--- nn_training/dataset.py
+++ nn_training/dataset.py
@@ -4,9 +4,9 @@ from torchvision.transforms import transforms
 
 
 class MatrixDataset(torch.utils.data.Dataset):
-    def __init__(self, data_df):
+    def __init__(self, data_df, reduced_degree):
         self.data_df = data_df.dropna().replace([True], 1).replace([False], 0)
-      #  self.transform = transforms.Compose([transforms.ToTensor()])
+        self.reduced_degree = reduced_degree
 
     def __len__(self):
         return len(self.data_df)
@@ -17,6 +17,18 @@ class MatrixDataset(torch.utils.data.Dataset):
         isEffective = sample.isEffective
 
         reduced = np.array([features.density,  features.nonzeros, features.nsym, features.posdef, features.psym, features.rows])
-      #  return self.transform(reduced), self.transform(isEffective)
-      #  return self.transform(reduced), self.transform(isEffective)
-        return reduced, isEffective
+        full = np.array([features.rows, features.nonzeros, features.posdef, features.nsym, features.psym, features.density,
+                         features.avg_nnz, features.max_nnz, features.std_nnz,
+                         features.avg_row_block_count, features.std_row_block_count, features.min_row_block_count, features.max_row_block_count,
+                         features.avg_row_block_size, features.std_row_block_size, features.min_row_block_size, features.max_row_block_size,
+                         features.block_count])
+        if self.reduced_degree == 3:
+            return reduced, isEffective
+        return full, isEffective
+
+    def get_amount_features(self):
+        if self.reduced_degree == 3:
+            return 6
+        if self.reduced_degree == 0:
+            return 18
+        return 18
--- nn_training/train.py
+++ nn_training/train.py
@@ -7,43 +7,46 @@ import pandas as pd
 from dataset import MatrixDataset
 import data_files
 
+reduced_feature = 0
+batch_size = 10
+
 df = pd.read_csv(data_files.DATASET_PATH).dropna()
 train_df = df.sample(frac=0.8)
 test_df = df.drop(train_df.index)
 print("train len: ", len(train_df))
 print("test len: ", len(test_df))
 test_df.to_csv("test_set.csv", index=False)
-train_set = MatrixDataset(train_df)
-test_set = MatrixDataset(train_df) #TODO ONLY FOR DEBUG
-batch_size = 2
+train_set = MatrixDataset(train_df, reduced_feature)
+test_set = MatrixDataset(test_df, reduced_feature)
 
 train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size,
                                            shuffle=True, pin_memory=True)
 val_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size,
                                          shuffle=False, pin_memory=True)
-# device = torch.device("cuda:2")
-# print("device_count: ", torch.cuda.device_count())
-# print("device: ", torch.cuda.get_device_name(0))
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Using {device} device")
+print("device_count: ", torch.cuda.device_count())
+print("device: ", torch.cuda.get_device_name())
 
 class Net(nn.Module):
-    def __init__(self):
+    def __init__(self, amount_features):
         super().__init__()
-        self.bn = nn.BatchNorm1d(6)
-        self.fc1 = nn.Linear(6, 14)
-        self.fc2 = nn.Linear(14, 1)
+        self.bn = nn.BatchNorm1d(amount_features)
+        self.fc1 = nn.Linear(amount_features, 256)
+        self.fc2 = nn.Linear(256, 1024)
+        self.fc3 = nn.Linear(1024, 1)
         self.sm = nn.Softmax(dim=0)
 
     def forward(self, x):
         x = self.bn(x)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
         x = self.sm(x)
         return x
 
-model = Net()
+model = Net(train_set.get_amount_features()).to(device)
 
 model.double()
 optimizer = optim.Adam(model.parameters(), lr=0.1)
@@ -63,17 +66,17 @@ def train(model, train_loader, num_epoch):
 
             optimizer.zero_grad()
 
-            outputs = model(inputs)
+            outputs = model(inputs.to(device))
             #print('Outputs: ', ' '.join(f'{outputs[j]}' for j in range(len(outputs))))
 
-            loss = criterion(outputs, labels)
+            loss = criterion(outputs, labels.to(device))
             loss.backward()
             optimizer.step()
 
             running_loss += loss.item()
-        print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss:.10f}')
+        if epoch % 100 == 1:
+            print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss:.10f}')
         loss_values.append(running_loss)
-       # loss_values.append(running_loss / len(train_set))
 
     print('Finished Training')
 
@@ -81,17 +84,20 @@ def train(model, train_loader, num_epoch):
     plt.show()
     return model
 
-def test(model, val_loader):
-    dataiter = iter(val_loader)
-    matrix_data, labels = dataiter.next()
+def test(matrix_data, labels):
     print('GroundTruth: ', ' '.join(f'{labels[j]}' for j in range(len(labels))))
 
-    outputs = model(matrix_data)
+    outputs = model(matrix_data.to(device))
     predicted = torch.squeeze(outputs.round().to(torch.int))
     print('Predicted: ', ' '.join(f'{predicted[j]}' for j in range(len(predicted))))
 
 
-model = train(model, train_loader, 20)
+model = train(model, train_loader, 200)
 torch.save(model.state_dict(), data_files.MODEL_PATH)
 
-test(model, val_loader)
+dataiter = iter(val_loader)
+matrix_data, labels = dataiter.next()
+#for i, data in enumerate(val_loader, 0):
+#    matrix_data, labels = data
+test(matrix_data, labels)
+
